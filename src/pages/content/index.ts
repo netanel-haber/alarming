84ee42c2e38dce7406fa16f035f145e28b6ad775
netanel-haber <netanel.haber@deci.ai>
2023-04-16 23:01:14 +0300

    cleanup

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,5 +1,32 @@
-try {
-  console.log('content script loaded');
-} catch (e) {
-  console.error(e);
+throw new Error("ejemplo");
+
+type ContentScriptMessage =
+  | {
+    type: "elementSelected";
+    element: string;
+  }
+  | {
+    type: "activateElementPicker";
+  };
+
+function onMouseDown(event: MouseEvent) {
+  event.stopPropagation();
+  event.preventDefault();
+  document.removeEventListener("mousedown", onMouseDown, true);
+
+  const selectedElement = event.target as HTMLElement;
+  chrome.runtime.sendMessage({
+    type: "elementSelected",
+    element: selectedElement.outerHTML,
+  });
 }
+
+console.log("Content script is running!");
+
+chrome.runtime.onMessage.addListener(
+  (message: ContentScriptMessage) => {
+    if (message.type === "activateElementPicker") {
+      document.addEventListener("mousedown", onMouseDown, true);
+    }
+  },
+);
